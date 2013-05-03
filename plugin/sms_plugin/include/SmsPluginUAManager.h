0d5fc94742c8aca31caf5990e9777a920857400c
Sangkoo Kim <sangkoo.kim@samsung.com>
2013-05-03 13:40:36 +0900

    Modify flora license version.
    Change-Id: I6a47a75e142c9e1ddb9cce23dcf5f29b00b4feef

--- plugin/sms_plugin/include/SmsPluginUAManager.h
+++ plugin/sms_plugin/include/SmsPluginUAManager.h
@@ -1,11 +1,11 @@
 /*
 * Copyright 2012-2013  Samsung Electronics Co., Ltd
 *
-* Licensed under the Flora License, Version 1.0 (the "License");
+* Licensed under the Flora License, Version 1.1 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-*    http://floralicense.org
+*    http://floralicense.org/license/
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
