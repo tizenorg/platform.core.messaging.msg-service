1253b79da47c03daf35aad28d70ac5f72b83ddb0
Kyeonghun Lee <kh9090.lee@samsung.com>
2016-04-27 16:27:59 +0900

    add boilerplate to msg-manager.cpp
    Change-Id: I966f04420a1b81ea29a059f543ce04112a2fe228
    Signed-off-by: Kyeonghun Lee <kh9090.lee@samsung.com>

--- manager/src/msg-manager.cpp
+++ manager/src/msg-manager.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/*==================================================================================================
+                                         INCLUDE FILES
+==================================================================================================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <glib.h>
@@ -23,10 +43,16 @@ extern "C"
 #include "msg-manager-sound.h"
 #include "msg-manager-util.h"
 
+/*==================================================================================================
+                                     DEFINES
+==================================================================================================*/
 /* below defines will be removed */
 #define EVENT_KEY_OUT_MSG_TYPE "msg_type"
 #define EVENT_KEY_OUT_MSG_ID "msg_id"
 
+/*==================================================================================================
+                                     VARIABLES
+==================================================================================================*/
 msg_handle_t msg_handle = NULL;
 cm_client_h cm_handle = NULL;
 
@@ -34,6 +60,9 @@ pthread_mutex_t mx;
 static int job_cnt = 0;
 static bool terminated = false;
 
+/*==================================================================================================
+                            FUNCTION IMPLEMENTATION
+==================================================================================================*/
 static int _service_app_exit(void *data)
 {
 	MSG_MGR_INFO("kill msg-manager");
